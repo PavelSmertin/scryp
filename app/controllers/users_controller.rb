3332115bcf103c72e54d2f0a5aab9d098d1524b1
pashtet <pashtetbezd@gmail.com>
2017-12-08 14:07:28 +0500

    + skip csrf on user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   include ErrorSerializer
   skip_before_action :authenticate, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     render json: User.all
