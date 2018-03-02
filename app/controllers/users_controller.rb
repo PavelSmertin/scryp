7e9dc04f5ed896fe214bde9de78c380a675bee98
pashtet <pashtetbezd@gmail.com>
2018-03-02 07:37:23 +0500

    синхронизация

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,49 +72,48 @@ class UsersController < ApplicationController
       portfolios_for_update << portfolio
     end
 
-    if (h["portfolio_coins"].nil?) do
+
+    if h["portfolio_coins"].nil? do
       render json: {success: true, updated_at: updated_at}, status: 200
-      return
+    else 
+      h["portfolio_coins"].each do |h_portfolio_coin|
+        created_at = DateTime.strptime(CGI::unescape(h_portfolio_coin["created_at"]), '%Y-%m-%d %H:%M:%S')
+
+        portfolio_coin = PortfolioCoin.where(user_id: current_user.id, portfolio_coin_id: h_portfolio_coin["_id"]).first_or_initialize
+
+        portfolio_coin.user_id           = current_user.id
+        portfolio_coin.portfolio_id      = h_portfolio_coin["portfolio_id"]
+        portfolio_coin.portfolio_coin_id = h_portfolio_coin["_id"]
+        portfolio_coin.coin_id           = h_portfolio_coin["coin_id"]
+        portfolio_coin.symbol            = h_portfolio_coin["symbol"]
+        portfolio_coin.exchange          = h_portfolio_coin["name"]
+        portfolio_coin.exchange_id       = h_portfolio_coin["exchange_id"]
+        portfolio_coin.original          = h_portfolio_coin["original"]
+        portfolio_coin.price_original    = h_portfolio_coin["price_original"]
+        portfolio_coin.change_24h        = h_portfolio_coin["change_24h"]
+        portfolio_coin.change_pct_24h    = h_portfolio_coin["change_pct_24h"]
+        portfolio_coin.removed           = h_portfolio_coin["removed"]
+        portfolio_coin.created_at        = created_at
+        portfolio_coin.updated_at        = updated_at
+
+        portfolio_coin.save
+
+      end
+
+      # portfolios_for_update.each { |portfolio|
+      #   calculator = Job::PortfolioCalculator.new
+      #   calculator.calculate_portfolio(current_user.id, portfolio)
+      # }
+
+      Job.run
+
+      if current_user.save
+        render json: {success: true, updated_at: updated_at}, status: 200
+      else
+        render json: ErrorSerializer.serialize(user.errors), status: 422
+      end
     end
-
-    h["portfolio_coins"].each do |h_portfolio_coin|
-
-      created_at = DateTime.strptime(CGI::unescape(h_portfolio_coin["created_at"]), '%Y-%m-%d %H:%M:%S')
-
-      portfolio_coin = PortfolioCoin.where(user_id: current_user.id, portfolio_coin_id: h_portfolio_coin["_id"]).first_or_initialize
-
-      portfolio_coin.user_id           = current_user.id
-      portfolio_coin.portfolio_id      = h_portfolio_coin["portfolio_id"]
-      portfolio_coin.portfolio_coin_id = h_portfolio_coin["_id"]
-      portfolio_coin.coin_id           = h_portfolio_coin["coin_id"]
-      portfolio_coin.symbol            = h_portfolio_coin["symbol"]
-      portfolio_coin.exchange          = h_portfolio_coin["name"]
-      portfolio_coin.exchange_id       = h_portfolio_coin["exchange_id"]
-      portfolio_coin.original          = h_portfolio_coin["original"]
-      portfolio_coin.price_original    = h_portfolio_coin["price_original"]
-      portfolio_coin.change_24h        = h_portfolio_coin["change_24h"]
-      portfolio_coin.change_pct_24h    = h_portfolio_coin["change_pct_24h"]
-      portfolio_coin.removed           = h_portfolio_coin["removed"]
-      portfolio_coin.created_at        = created_at
-      portfolio_coin.updated_at        = updated_at
-
-
-      portfolio_coin.save
-
-    end
-
-    # portfolios_for_update.each { |portfolio|
-    #   calculator = Job::PortfolioCalculator.new
-    #   calculator.calculate_portfolio(current_user.id, portfolio)
-    # }
-
-    Job.run
-
-    if current_user.save
-      render json: {success: true, updated_at: updated_at}, status: 200
-    else
-      render json: ErrorSerializer.serialize(user.errors), status: 422
-    end
+    
   end
 
   def data
